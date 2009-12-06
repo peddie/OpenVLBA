22a66bcc950e079534c2f8eacaaded51170e2232
Matthew Peddie <mpeddie@gmail.com>
2009-12-06 00:51:00 -0800

    a little bit of vector math

--- Main.hs
+++ Main.hs
@@ -1,2 +1,58 @@
 module Main where
 
+import System.Random.Mersenne
+
+data Vector3 = Vector3 { x :: Double 
+                       , y :: Double
+                       , z :: Double 
+                       } deriving (Show)
+
+-- equality
+instance Eq Vector3 where
+    (Vector3 x1 y1 z1) == (Vector3 x2 y2 z2) = and [(x1 == x2), (y1 == y2), (z1 == z2)]
+
+-- Cross product, addition
+instance Num Vector3 where
+    (Vector3 x1 y1 z1) * (Vector3 x2 y2 z2) = Vector3 (y1*z2) (z1*x2) (x1*y2)
+    (Vector3 x1 y1 z1) + (Vector3 x2 y2 z2) = Vector3 (x1+x2) (y1+y2) (z1+z2)
+    (Vector3 x1 y1 z1) - (Vector3 x2 y2 z2) = Vector3 (x1-x2) (y1-y2) (z1-z2)
+    fromInteger x = Vector3 (fromInteger x) (fromInteger x) (fromInteger x)
+
+{-
+instance Fractional Vector3 where
+    fromRational x = Vector3 (fromRational x) (fromRational y) (fromRational z)
+-}
+
+mag :: Vector3 -> Double
+mag (Vector3 x y z) = sqrt $ sum $ map (^2) [x, y, z]
+
+hat :: Vector3 -> Vector3
+hat v@(Vector3 x y z) = Vector3 (x / m) (y / m) (z / m)
+                      where m = mag v
+
+-- Dot product
+(.*) :: Vector3 -> Vector3 -> Double
+(Vector3 x1 y1 z1) .* (Vector3 x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
+
+makeSignal :: Double -> Double -> Double -> [Double]
+makeSignal frequency timestep starttime = map sin $ map (2 * pi * frequency *) [starttime,(starttime+timestep)..]
+
+data Station = Station { pos :: Vector3
+                         , timing :: Double 
+                       } deriving (Show)
+
+-- timing means frequency for a Source and sample time for a Receiver
+
+type Receiver = Station
+type Source = Station
+
+c = 2.9979e8
+
+receive :: Source -> Receiver -> [Double]
+receive src@(Station srcpos freq) recv@(Station recvpos timestep) = makeSignal freq timestep delay
+    where 
+      dist = mag $ recvpos - srcpos
+      delay = dist / c
+
+noiseify :: Double -> [Double] -> [IO Double]
+noiseify variance signal = zipWith (+) signal (repeat (randomIO :: IO Double))

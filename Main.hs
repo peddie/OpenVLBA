beeb90dde6bad280714cb4ee647d8828fb72a73c
Fergus Noble <fergusnoble@gmail.com>
2009-12-06 21:32:24 -0800

    started work on correlation

--- Main.hs
+++ Main.hs
@@ -39,31 +39,59 @@ hat v@(Vector3 x y z) = Vector3 (x / m) (y / m) (z / m)
 makeSignal :: Double -> Double -> Double -> [Double]
 makeSignal frequency timestep starttime = map sin $ map (2 * pi * frequency *) [starttime,(starttime+timestep)..]
 
+-- create stream of uniformly distributed random number on [0,1)
+uniformnoise :: Int -> [Double]
+uniformnoise seed = randoms (mkStdGen seed)
+
+-- Create stream of gaussian distributed random numbers applying a Box-Muller transform 
+-- to an input stream of uniformly distributed random numbers 
+boxmuller (x:y:rands) = sqrt (-2 * log x) * cos (2*pi*y) : sqrt (-2 * log x) * sin (2*pi*y) : boxmuller rands
+
+-- stream of random numbers with gaussian distn. with specified variance
+gaussiannoise variance = map ((sqrt variance) *) $ boxmuller $ uniformnoise 22
+
+-- additive white gaussian noise (AWGN) channel model with variance specified applied to signal
+noiseify variance signal = zipWith (+) signal $ gaussiannoise variance
+
 data Station = Station { pos :: Vector3
                          , timing :: Double 
                        } deriving (Show)
 
 -- timing means frequency for a Source and sample time for a Receiver
-
 type Receiver = Station
 type Source = Station
 
-c = 2.9979e8
+c = 3e8
+
+stationtiming (Station pos timing) = timing
+stationpos (Station pos timing) = pos
+
+stationdist a b = mag $ stationpos a - stationpos b
+stationdelay a b = stationdist a b / c
 
 receive :: Source -> Receiver -> [Double]
 receive src@(Station srcpos freq) recv@(Station recvpos timestep) = makeSignal freq timestep delay
-    where 
-      dist = mag $ recvpos - srcpos
-      delay = dist / c
+    where
+      delay = stationdelay src recv
+      
+receivenoisy variance src recv = noiseify variance $ receive src recv
 
-randomnoise :: Int -> [Double]
-randomnoise seed = randoms (mkStdGen seed)
+correlation 0 as bs = 0
+correlation length (a:as) (b:bs) = a*b + correlation (length-1) as bs
 
-gaussiannoise (x:y:rands) = sqrt ((-2 * log x)) * cos(2*pi*y):gaussiannoise rands
+crosscorrelation length as (b:bs) = correlation length as (b:bs) : crosscorrelation length as bs
 
-mkgaussiannoise = gaussiannoise $ randomnoise 22
+-- TEST DATA
+r_a :: Receiver
+r_a = Station { pos = Vector3 50000 0 0, timing = 1e-6}
+r_b :: Receiver
+r_b = Station { pos = Vector3 0 50000 0, timing = 1e-6}
+r_c :: Receiver
+r_c = Station { pos = Vector3 50000 50000 0, timing = 1e-6}
 
-noiseify signal variance = zipWith (+) signal $ map ((sqrt variance) *) mkgaussiannoise
+s_a :: Source
+s_a = Station { pos = Vector3 0 0 50000, timing = 1e3}
 
-plotsignal :: (Double t, PlotWindowType ([t] -> t1)) => t1
-plotsignal signal = plotWindow [1..100] $ take 100 signal
+range :: Double
+range = 1000.0
+plotsignal name signal = plotPDF (name ++ ".pdf") [1..range] $ take 1000 signal
